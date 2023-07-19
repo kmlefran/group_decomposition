df87b4b13b1ed42a504876c19a2c57e6eab58936
kmlefran <kgagnon@lakeheadu.ca>
2023-07-19 11:29:45 -0400

    Update version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 setuptools.setup(name='group_decomposition',
-version='0.0.1',
+version='0.1.0',
 description='Extract fragments and counts of fragments from molecule SMILES codes',
 url='https://github.com/kmlefran/group_decomposition',
 author='Kevin M. Lefrancois-Gagnon',
