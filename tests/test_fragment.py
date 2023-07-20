44d083bf009165fa7c1717780de2f560bf0f17cc
kmlefran <kgagnon@lakeheadu.ca>
2023-07-20 14:09:41 -0400

    cleaned up tests

--- tests/test_fragment.py
+++ tests/test_fragment.py
@@ -8,21 +8,21 @@ import pandas as pd
 
 # class TestFragmenting(unittest.TestCase):
 def test_two_ethyls_in_smile():
+    """We should find two ethyls in this molecule"""
     smi='c1ccc(c(c1)c2ccc(o2)C(=O)N3C[C@H](C4(C3)CC[NH2+]CC4)C(=O)NCCOCCO)F'
     atframe = identify_connected_fragments(smi)
-
     uniqueframe = count_uniques(atframe)
-    # print(list(uniqueframe[uniqueframe['Smiles']=='*CC*']['count'])[0])
     numEt = list(uniqueframe[uniqueframe['Smiles']=='*CC*']['count'])[0]
-    # self.assertEqual(numEt,2,'should be 2 ethyl')
     assert numEt == 2
 
 
 def test_invalid_smiles_raises_ValueError():
+    """Given an invalid smiles, we should see a ValueError"""
     with pytest.raises(ValueError):
         identify_connected_fragments('CBx')
 
 def test_phenyl_in_smi():
+    """Test that we find a phenyl ring in the molecule"""
     frag_frame = identify_connected_fragments('c1ccc(cc1)C[C@H]2CC[C@@]3(C2)[C@@H](CCCN3Br)O')
     unique_frame = count_uniques(frag_frame)
     frag_smiles = list(unique_frame['Smiles'])
@@ -44,9 +44,5 @@ def test_acyclic_molecule_is_fragmented():
 def test_two_phenyl_counted_same_if_drop_attachements():
     frag_frame = identify_connected_fragments('c1cc(ccc1O)Oc2c(ccc(c2F)F)[N+](=O)[O-]')
     unique_frame = count_uniques(frag_frame,drop_attachments=True)
-    # print(unique_frame)
-    # print(list(unique_frame[unique_frame['Smiles']]))
     num_phenyl = list(unique_frame[unique_frame['Smiles']=='c1ccccc1']['count'])[0]
     assert num_phenyl == 2
-# if __name__ == '__main__':
-#     unittest.main()
