b642a65407d718738340c8ae3bc75af603ab9265
kmlefran <kgagnon@lakeheadu.ca>
2023-09-19 14:02:18 -0400

    Package restructure

---
+++ docs/source/conf.py
@@ -0,0 +1,62 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'group_decomposition'
+copyright = '2023, Kevin M. Lefrancois-Gagnon and Robert C. Mawhinney'
+author = 'Kevin M. Lefrancois-Gagnon and Robert C. Mawhinney'
+
+# The full version, including alpha/beta/rc tags
+release = '0.3.17'
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+import os 
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+import pathlib
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+html_theme = "alabaster"
+html_static_path = ["_static"]
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. group_decomposition documentation master file, created by
+   sphinx-quickstart on Tue Sep 19 13:52:11 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to group_decomposition's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
